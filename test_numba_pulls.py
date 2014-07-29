dbd2053e83df8f014e78602f3fe89fb26893fb2c
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-07-29 16:41:02 -0500

    Do merge

--- test_numba_pulls.py
+++ test_numba_pulls.py
@@ -18,34 +18,6 @@ test_label = 'test_' + platform
 ghuser = 'sklam'  # put your user name here
 ghpass = getpass('enter password for %s > ' % ghuser)
 
-meta_yaml = """
-package:
-  name: numba
-  version: testing
-
-source:
-  git_url: %(URL)s
-  git_tag: %(TAG)s
-
-requirements:
-  build:
-    - numpy
-    - python
-  run:
-    - python
-    - argparse        [py26]
-    - numpy
-    - llvmpy
-    - cudatoolkit
-
-test:
-  requires:
-    - unittest2      [py26]
-  commands:
-    - pycc -h
-    - numba -h
-"""
-
 def do_testing(stdout):
     PY = ['26', '27', '33']
     NP = ['17', '18']
@@ -55,6 +27,20 @@ def do_testing(stdout):
         print("==", py, np)
         subprocess.check_call("conda build --no-binstar-upload numba_template".split(),
                                stdout=stdout, stderr=subprocess.STDOUT)
+
+build_sh_template = """#!/bin/bash
+%s
+$PYTHON setup.py install
+"""
+
+bld_bat_template = """%s
+%%PYTHON%% setup.py install
+if errorlevel 1 exit 1
+
+if "%%PY3K%%"=="1" (
+    rd /s /q %%SP_DIR%%\numpy
+)
+"""
 
 def run():
     print("=== Run ===")
@@ -79,7 +65,8 @@ def run():
             head = data['head']
             branch = head['ref']
             clone_url = head['repo']['clone_url']
-            branches.append((iss.number, branch, clone_url))
+            if data['mergeable']:
+                branches.append((iss.number, branch, clone_url))
     else:
         if not branches:
             print("Nothing to do")
@@ -93,8 +80,14 @@ def run():
             return iss
 
         print("==", issnum, branch, url, "==")
-        with open("numba_template/meta.yaml", 'w') as fyaml:
-            fyaml.write(meta_yaml % dict(URL=url, TAG=branch))
+
+        get_master = "git clone https://github.com/numba/numba.git ."
+        pull_remote = "git pull %s %s" % (url, branch)
+        cmds = '\n'.join([get_master, pull_remote])
+        with open("numba_template/build.sh", 'w') as fscript:
+            fscript.write(build_sh_template % cmds)
+        with open("numba_template/bld.bat", 'w') as fscript:
+            fscript.write(bld_bat_template % cmds)
 
         stdout = tempfile.TemporaryFile()
         try:
